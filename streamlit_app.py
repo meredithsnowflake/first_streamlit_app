f175ad8b17f16ae0bb6faec47050ae0ea45c5e10
meredithsnowflake <138932786+meredithsnowflake@users.noreply.github.com>
2023-07-28 15:55:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,10 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#This will not work correctly
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
